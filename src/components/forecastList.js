8c2dd27fcb32dba7e9f57317cdebc8f9b08350cd
Liam Baker <liam.baker2310@gmail.com>
2021-03-26 19:06:18 +0900

    refactored scroll forecast function

--- src/components/forecastList.js
+++ src/components/forecastList.js
@@ -6,35 +6,23 @@ const ForecastList = props => {
   const { forecast, type } = props;
 
   const [ arr, setArr ] = useState([]);
-  const [ activeArr, updateActiveArr ] = useState([]);
+  const [ activeArr, setActiveArr ] = useState([]);
 
   useEffect(() => {
     setArr(forecast);
-    updateActiveArr(forecast.slice(0,3))
+    setActiveArr(forecast.slice(0,3))
   }, [forecast])
 
   const handleClick = event => {
+    let idx, newArr;
     if (event.currentTarget.classList.contains('left-arrow')) {
-      handleLeft(event)
+      idx = arr.indexOf(activeArr[0]) - 1
+      newArr = arr.slice(idx, (idx + 3))
     } else {
-      handleRight(event)
+      idx = arr.indexOf(activeArr[2]) + 2
+      newArr = arr.slice((idx - 3), idx)
     }
-  }
-
-  const handleLeft = event => {
-    const temp = [...activeArr]
-    temp.pop()
-    let idx = arr.indexOf([...temp].shift())
-    temp.unshift(arr[idx - 1])
-    updateActiveArr(temp)
-  }
-
-  const handleRight = event => {
-    const temp = [...activeArr]
-    temp.shift()
-    let idx = arr.indexOf([...temp].pop())
-    temp.push(arr[idx + 1])
-    updateActiveArr(temp)
+    setActiveArr(newArr);
   }
 
   const leftArrowClass = activeArr[0] === arr[0] ? 'left-arrow h-8 w-8 invisible' : 'left-arrow h-8 w-8 cursor-pointer'
